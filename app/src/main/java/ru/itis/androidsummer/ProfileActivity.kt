1d5691d1959a62a53bfe335c9f7e14f74f70140a
ddddi21 <dilyra.as@gmail.com>
2020-07-14 18:53:24 +0300

    fix profile + add setting + add in SP something + remove "0" on button

--- app/src/main/java/ru/itis/androidsummer/ProfileActivity.kt
+++ app/src/main/java/ru/itis/androidsummer/ProfileActivity.kt
@@ -3,13 +3,16 @@ package ru.itis.androidsummer
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.itis.androidsummer.SplashActivity.Companion.APP_PREFERENCES
 import ru.itis.androidsummer.SplashActivity.Companion.APP_PREFERENCES_REGISTRATION
-import ru.itis.androidsummer.SplashActivity.Companion.APP_PREFERENCES_SCORE
+import ru.itis.androidsummer.SplashActivity.Companion.APP_PREFERENCES_WHOLE_SCORE
+import ru.itis.androidsummer.SplashActivity.Companion.APP_PREFERENCES_VICTORY
 
 class ProfileActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
@@ -17,24 +20,45 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         val sharedPreferences = getSharedPreferences(APP_PREFERENCES,Context.MODE_PRIVATE)
+        val score = sharedPreferences.getInt(APP_PREFERENCES_WHOLE_SCORE,0)
 
-        tv_profile_name.text = "Твое имя: "+ sharedPreferences.getString(APP_PREFERENCES_REGISTRATION,
+        tv_profile_name.text = sharedPreferences.getString(APP_PREFERENCES_REGISTRATION,
             resources.getString(R.string.profile_text_default_name)
         )
-        tv_profile_score.text = "Количество очков:"+ sharedPreferences.getInt(APP_PREFERENCES_SCORE,
-            0
-        ).toString()
-        btn_profile_sign_Out.setOnClickListener {
-            startActivity(Intent(this, RegistrationActivity::class.java))
-            finish()
+        tv_profile_victory_count.text = sharedPreferences.getInt(APP_PREFERENCES_VICTORY,0).toString()
+//        tv_profile_score.text = sharedPreferences.getInt(APP_PREFERENCES_SCORE,
+//            0
+//        ).toString()
+        //нужно будет сохранять количество очков за игру(после завершения)
+        // и выводить сюда(они будут постоянными, просто накапливаться), be like:
+        if (score == 0) {
+            tv_profile_score.text = sharedPreferences.getString(
+                APP_PREFERENCES_WHOLE_SCORE,
+                resources.getString(R.string.profile_text_default_score)
+            )
+        } else
+            tv_profile_score.text = score.toString()
+
+//        tv_profile_place_in_rating.text = //доставать откуда то место в рейтинге
+
+
+//        tv_profile_name_victory.text = // из SP надо доставать колво побед и в профиль выводить(нужна сеть)
+
+        tv_profile_name.underline()
+        tv_profile_score.underline()
+        tv_profile_victory_count.underline()
+        tv_profile_place_in_rating.underline()
+
+        iv_profile_back_to_menu.setOnClickListener{
+            startActivity(Intent(this,MainMenuActivity::class.java))
         }
-        btn_profile_reset_score.setOnClickListener {
-            sharedPreferences.edit().putInt(APP_PREFERENCES_SCORE,0).apply()
-            btn_profile_reset_score.text = "0"
-            Toast.makeText(applicationContext, R.string.profile_button_score_reset_notification,
-                Toast.LENGTH_LONG).show()
-            finish()
+
+        iv_profile_rating.setOnClickListener {
+            //переход на страничку с рейтингом, которого пока нет
         }
+
     }
-    //тут я кароче еще менять буду, тут ненормально
+    fun TextView.underline() {
+        paintFlags = paintFlags or Paint.UNDERLINE_TEXT_FLAG
+    }
 }
---
+++ app/src/main/java/ru/itis/androidsummer/SettingsActivity.kt
@@ -0,0 +1,47 @@
+package ru.itis.androidsummer
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_settings.*
+import ru.itis.androidsummer.SplashActivity.Companion.APP_PREFERENCES_WHOLE_SCORE
+
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val sharedPreferences = getSharedPreferences(SplashActivity.APP_PREFERENCES, Context.MODE_PRIVATE)
+        val score = sharedPreferences.getInt(APP_PREFERENCES_WHOLE_SCORE,0)
+
+        btn_settings_sign_Out.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+            finish()
+        }
+
+        btn_settings_reset_score.setOnClickListener {
+            if(score!=0) {
+                sharedPreferences.edit().putInt(APP_PREFERENCES_WHOLE_SCORE, 0).apply()
+                Toast.makeText(
+                    this, R.string.profile_button_score_reset_notification,
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            } else {
+                Toast.makeText(
+                    this,"У тебя уже 0 очков!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        iv_settings_back_to_menu.setOnClickListener {
+            startActivity(Intent(this,MainMenuActivity::class.java))
+        }
+        //когда сеть будет, отсюда можно что нибудь с хостами я хз
+        //можно добавить смену на темную тему
+    }
+}
