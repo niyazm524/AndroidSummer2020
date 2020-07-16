4621c47057abca949a131365ee8e664b4a032e03
Galiev Bulat <Kakadurf@gmail.com>
2020-07-16 12:57:21 +0300

    testestest

---
+++ app/src/main/java/ru/itis/androidsummer/Test.kt
@@ -0,0 +1,29 @@
+package ru.itis.androidsummer
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.test.*
+import ru.itis.androidsummer.data.Question
+import ru.itis.androidsummer.parsers.ContentsXmlParser
+import java.lang.Exception
+
+class Test : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.test)
+        try{
+            Glide.with(this).load(BitmapFactory.decodeStream(
+                ContentsXmlParser.hashThing.get(Question(300," ","Rhino",false)))).into(imageView)
+            /*
+        imageView.setImageBitmap(BitmapFactory.decodeStream(
+            ContentsXmlParser.hashThing.get(Question(300," ","Rhino",false))))*/
+        } catch (e: Exception){
+            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
